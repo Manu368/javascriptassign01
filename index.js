4a9fdadc63909aa05ffcdf8ca1afb557cff4b753
Manu368 <116960480+Manu368@users.noreply.github.com>
2023-01-30 23:35:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+alert("Welcom to My Assignment");
+function manu01() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("heading01").style.color = "white";
+    document.getElementById("heading02").style.color = "white";
+    document.getElementById("paragraph01").style.color = "white";
+    document.getElementById("horizontal").style.backgroundColor = "white";
+
+}
+function manu02() {
+    document.body.style.backgroundColor = "grey";
+    document.getElementById("heading01").style.color = "black";
+    document.getElementById("heading02").style.color = "black";
+    document.getElementById("paragraph01").style.color = "black";
+    document.getElementById("horizontal").style.backgroundColor = "black";
+
+}
+function manu03() {
+    document.body.style.backgroundColor = "white";
+    document.getElementById("heading01").style.color = "black";
+    document.getElementById("heading02").style.color = "black";
+    document.getElementById("paragraph01").style.color = "black";
+    document.getElementById("horizontal").style.backgroundColor = "black";
+
+}
